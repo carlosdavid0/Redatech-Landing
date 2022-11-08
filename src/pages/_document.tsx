deaf321c367bffc63784e35d8c8240f564fa0843
Carlos David <romao.carlosdavi@gmail.com>
2022-11-08 20:56:21 -0300

    :sparkles: final

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,7 @@ export default function Document() {
     return (
         <Html lang='pt-br'>
             <Head>
-                <title>Redatech</title>
+
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='' />
                 <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet" />
