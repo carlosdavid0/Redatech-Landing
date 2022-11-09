3ce1ac675c77c054142cff929d43e787e237687c
Carlos David <romao.carlosdavi@gmail.com>
2022-11-09 18:20:38 -0300

    :wrench: ajustes na landing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,83 +16,52 @@ import { Head } from 'next/document'
 
 
 export default function index() {
-  const [email, setEmail] = useState('')
-  const [loading, setLoading] = useState(false)
 
   const db = getFirestore(firebaseConfig)
   const userCollection = collection(db, 'users')
   const notify = (message: string, type: 'success' | 'error') => toast(message, { type });
 
-  async function addUser(event: FormEvent) {
 
-    event.preventDefault()
-    setLoading(true)
-
-    try {
-
-      await addDoc(userCollection, {
-        email: email
-      })
-
-      notify('Email cadastrado com sucesso!', 'success')
-
-      setLoading(false)
-      setEmail('')
-
-    } catch (error) {
-      setLoading(false)
-      notify('Erro ao cadastrar email!', 'error')
-      console.log(error)
-    } finally {
-      setLoading(false)
-    }
-
-
-
-
-
-  }
 
   return (
 
     <>
-      <div className="lg:max-w-[1124px] h-screen lg:mx-auto justify-center grid lg:grid-cols-2 grid-cols-1 mx-10 items-center gap-28">
-
-        <main>
+      <div className="lg:max-w-[1124px] h-screen lg:mx-auto  flex justify-center flex-col mx-10">
+        <header className="flex items-center justify-between mt-10">
           <Image src={Logo} alt='Redatech logo' />
-
-          <h1 className='mt-14 text-5xl text-white leading-tight font-bold ' >
-            Desenvolva suas habilidades em redação e pontue alto no ENEM.
-          </h1>
-
-          <form className='mt-10 flex gap-2' onSubmit={addUser}>
-            <input
-              required
-              onChange={event => setEmail(event.target.value)}
-              value={email}
-              type="email"
-              placeholder='Digite seu melhor e-mail'
-              className='flex-1 px-6 py-4 rounded bg-gray-800 border border-gray-600 text-sm text-gray-100'
-            />
+          <div className='flex gap-2 '>
             <button
               type="submit"
-              disabled={loading}
-              className=' px-6 py-4 font-bold text-sm uppercase rounded text-white bg-orange-500 hover:bg-orange-900 disabled:bg-orange-800'
-            > {loading ? (
-              <div className='animate-spin rounded-full h-5 w-5 border-t-2 border-b-2 border-gray-100'></div>
-            ) : 'Cadastrar'} </button>
-          </form >
+              className=' px-6 py-4 font-bold text-sm uppercase rounded text-white bg-orange-500 hover:bg-orange-900 disabled:bg-orange-800'>Login </button>
 
+            <button
+              type="submit"
+              className='hidden lg:block px-6 py-4 font-bold text-sm uppercase rounded text-white bg-redatech-500 hover:bg-redatech-900 disabled:bg-redatech-800'>Sign In </button>
 
-          <p className='mt-4 text-gray-400 text-sm leading-relaxed'>Coloque seu melhor e-mail para entrarmos em contato assim que tivermos alguma atualização.</p>
+          </div>
+        </header>
+
+        <main className='grid lg:grid-cols-2 grid-cols-1 items-center gap-28 mx-auto'>
+
+          <section className=''>
+            <h1 className='mt-5 lg:mt-14 text-5xl text-white leading-tight font-bold ' >
+              Desenvolva suas habilidades em redação e pontue alto no ENEM.
+            </h1>
+
+            <p className='lg:mt-10 mt-4 text-white text-xl text-justify'><span className='text-bold text-orange-500'>+ 300</span> alunos consumiram esse conteúdo </p>
+
+            <button
+              type="submit"
+              className='mt-4 px-6 py-4 font-bold text-sm uppercase rounded text-white bg-orange-500 hover:bg-orange-900 disabled:bg-orange-800'>Entrar </button>
+
+          </section>
+
+          <Image src={Preview} alt='Homem conectando um cabo usb em um cérebro humano' priority className='hidden lg:block' />
+
         </main >
 
-        <Image src={Preview} alt='Homem conectando um cabo usb em um cerebro humano' priority className='hidden lg:block' />
-
-
-
       </div >
-      <ToastContainer autoClose={5000} />
+      <ToastContainer autoClose={5000} hideProgressBar />
     </>
 
   )
